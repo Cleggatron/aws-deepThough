bd87cbf62184cddaf07a6d1c05527c3795bf0e4a
Cleggatron <david.robert.clegg@gmail.com>
2022-01-05 21:37:22 +0000

    create dynamo db table

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -1,31 +1,36 @@
+//import aws sdk
 const AWS = require("aws-sdk");
 
+//update our config. local host requires dynamo db working in other terminal
 AWS.config.update({
     region: "us-east-2",
     endpoint: "http://localhost:8000"
 });
 
+//create dynamo db
 const dynamodb =  new AWS.DynamoDB({apiVersion: "2012-08-10"});
 
+//key schema for table
 const params = {
-    TableName: "Thoughts",
-    KeySchema: [
-        {AttributeName: "username", KeyType: "HASH"}, //partition key
-        {AttributeName: "createdAt", KeyType: "RANGE"} //sort key
+    TableName : "Thoughts",
+    KeySchema: [       
+      { AttributeName: "username", KeyType: "HASH"},  // Partition key
+      { AttributeName: "createdAt", KeyType: "RANGE" }  // Sort key
     ],
-    AttributeDefinitions: [
-        {AttributeName: "username", AttrbuteType: "S"},
-        {AttributeName: "createdAt", AttrbuteType: "N"},
+    AttributeDefinitions: [       
+      { AttributeName: "username", AttributeType: "S" },
+      { AttributeName: "createdAt", AttributeType: "N" }
     ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 10,
-        WriteCapacityUnits: 10
+    ProvisionedThroughput: {       
+      ReadCapacityUnits: 10, 
+      WriteCapacityUnits: 10
     }
 };
 
+//create our table
 dynamodb.createTable(params, (err, data) => {
     if(err){
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2))
     } else{
         console.log("Created table. Table Description JSON:", JSON.stringify(data, null, 2));
     }
